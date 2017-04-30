2763b54f18a4993ed17e412b0a64ff7ca57ee095
Volha Kryutsova <olgakriwtsowa@gmail.com>
2017-04-30 16:39:20 +0200

    Aufgabe 3 fertig

--- aufgabe1.js
+++ aufgabe1.js
@@ -8,11 +8,38 @@ fs.readFile(__dirname+"/staedte.json", function(err, data) {
     var s = JSON.parse(data);
 
     for(var i = 0; i < s.cities.length; i++){
-        console.log(chalk.blue("name: "+s.cities[i].name));
+        console.log(chalk.red("name: "+s.cities[i].name));
         console.log(chalk.red("country: "+s.cities[i].country));
-        console.log(chalk.yellow("population: "+s.cities[i].population));
+        console.log(chalk.red("population: "+s.cities[i].population));
         console.log("\n------------------------\n")
     }
 
 
+      s.cities.sort(function(a, b){
+        if(a.population>b.population){
+          return -1;
+        }
+        else if(a.population<b.population){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+      });
+
+      for(var j=0; j<s.cities.length; j++){                                                   //Ausgabe von dem was in staedte_sortiert.json geschrieben wird (optional)
+        console.log(chalk.green("name: "+s.cities[j].name));
+        console.log(chalk.green("country: "+s.cities[j].country));
+        console.log(chalk.green("population: "+s.cities[j].population));
+        console.log("\n------------------------\n")
+      };
+
+
+      fs.writeFile(__dirname + "/staedte_sortiert.json", JSON.stringify(s), function(err){    //stringify wandelt die Daten in ein lesbaren String um
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Datei erfolgreich gespeichert');
+        }
+  });
 });
